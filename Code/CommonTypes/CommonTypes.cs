17ed4e539a47188464e4082ff5500d3e0eb719bd
Lénio Bianchi Passos <ist426300@WIN.TAGUS.IST.UTL.PT>
2018-11-04 18:37:04 +0000

    Puppet Master can start server in LPC

---
+++ Code/CommonTypes/CommonTypes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_DAD
+{
+    public class MyRemoteObject : MarshalByRefObject
+    {
+
+        private Dictionary<string, Process> processes = new Dictionary<string, Process>();
+        public const string serverPath = "..\\..\\..\\ConsoleApp1\\bin\\Debug\\ConsoleApp1.exe";
+        //private Dictionary<string, string> urlByPid = new Dictionary<string, string>();
+        //private Dictionary<string, ISlaveControl> slaveByPid = new Dictionary<string, ISlaveControl>();
+
+        public void StartServer(string id, string min_delay, string max_delay)
+        {
+            Console.WriteLine("# StartServer:");
+
+
+            if (processes.ContainsKey(id))
+            {
+                if (processes[id].HasExited) //If process is terminated
+                {
+                    processes.Remove(id);
+                    //urlByPid.Remove(id);
+                }
+            }
+            if (!processes.ContainsKey(id))
+            {
+                try
+                {
+                    Process p = new Process();
+                    p.StartInfo.FileName = serverPath;
+                    // endpoint msec numPlayers 
+                    //p.StartInfo.Arguments = server_url + " " + msec + " " + num_players;
+                    p.Start();
+                    processes.Add(id, p);
+                    //urlByPid.Add(id, url); slaveByPid.Add(pid, (ISlaveControl)Activator.GetObject(typeof(ISlaveControl), urlByPid[pid]));
+                }
+                catch (InvalidOperationException) { Console.WriteLine("FileName specified is not valid"); }
+                catch (Win32Exception e) { Console.WriteLine("Couldn't Initialize the Server"); Console.WriteLine(e); }
+            }
+            else
+                Console.WriteLine("\nThe pid specified already exists : {0}", id);
+        }
+    }
+}
+
--- Code/GUI_User/PuppetMaster.Designer.cs
+++ Code/GUI_User/PuppetMaster.Designer.cs
@@ -41,18 +41,20 @@
             // textBox_Browse
             // 
             this.textBox_Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_Browse.Location = new System.Drawing.Point(345, 24);
+            this.textBox_Browse.Location = new System.Drawing.Point(259, 20);
+            this.textBox_Browse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_Browse.Name = "textBox_Browse";
-            this.textBox_Browse.Size = new System.Drawing.Size(536, 24);
+            this.textBox_Browse.Size = new System.Drawing.Size(403, 21);
             this.textBox_Browse.TabIndex = 0;
             // 
             // button_Browse
             // 
             this.button_Browse.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button_Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Browse.Location = new System.Drawing.Point(211, 18);
+            this.button_Browse.Location = new System.Drawing.Point(158, 15);
+            this.button_Browse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_Browse.Name = "button_Browse";
-            this.button_Browse.Size = new System.Drawing.Size(128, 35);
+            this.button_Browse.Size = new System.Drawing.Size(96, 28);
             this.button_Browse.TabIndex = 1;
             this.button_Browse.Text = "Browse";
             this.button_Browse.UseVisualStyleBackColor = false;
@@ -62,28 +64,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.Size = new System.Drawing.Size(140, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Select the script";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(12, 146);
+            this.textBox2.Location = new System.Drawing.Point(9, 119);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(1003, 159);
+            this.textBox2.Size = new System.Drawing.Size(753, 130);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Location = new System.Drawing.Point(9, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 25);
+            this.label2.Size = new System.Drawing.Size(148, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Write a command";
             // 
@@ -91,12 +97,14 @@
             // 
             this.button_Run.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.button_Run.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Run.Location = new System.Drawing.Point(887, 95);
+            this.button_Run.Location = new System.Drawing.Point(665, 77);
+            this.button_Run.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_Run.Name = "button_Run";
-            this.button_Run.Size = new System.Drawing.Size(128, 35);
+            this.button_Run.Size = new System.Drawing.Size(96, 28);
             this.button_Run.TabIndex = 5;
             this.button_Run.Text = "Run";
             this.button_Run.UseVisualStyleBackColor = false;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
             // 
             // openFileDialog1
             // 
@@ -107,9 +115,10 @@
             this.button_Send.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button_Send.Enabled = false;
             this.button_Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Send.Location = new System.Drawing.Point(887, 18);
+            this.button_Send.Location = new System.Drawing.Point(665, 15);
+            this.button_Send.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_Send.Name = "button_Send";
-            this.button_Send.Size = new System.Drawing.Size(128, 35);
+            this.button_Send.Size = new System.Drawing.Size(96, 28);
             this.button_Send.TabIndex = 6;
             this.button_Send.Text = "Send";
             this.button_Send.UseVisualStyleBackColor = false;
@@ -117,9 +126,9 @@
             // 
             // PuppetMaster
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1021, 317);
+            this.ClientSize = new System.Drawing.Size(766, 258);
             this.Controls.Add(this.button_Send);
             this.Controls.Add(this.button_Run);
             this.Controls.Add(this.label2);
@@ -127,6 +136,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button_Browse);
             this.Controls.Add(this.textBox_Browse);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PuppetMaster";
             this.Text = "PuppetMaster";
             this.Load += new System.EventHandler(this.GUI_Client_Load);
