38b4e48f4d6b83ec146e14010125b93a015510dc
Lénio Bianchi Passos <ist426300@win.tagus.ist.utl.pt>
2018-11-14 04:49:58 +0000

    Espaço de Tuplos feito

---
+++ AllCodes/Code_test_version/TupleSpace/TupleSpace/ObjectsSupoortedInTupleSpace.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace TupleSpace
+{
+    
+
+    [Serializable]
+    public class DADTestA
+    {
+        public int i1;
+        public string s1;
+
+        public DADTestA(int pi1, string ps1)
+        {
+            i1 = pi1;
+            s1 = ps1;
+        }
+        public override bool Equals(object o)
+        {
+
+            StringEmulator sm = o as StringEmulator; //Test WILDCARD
+            if (sm != null)
+            {
+                if( sm.GetString().Equals("*"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            DADTestA a = o as DADTestA;
+            if (a == null)
+            {
+                return false;
+            }
+            else
+            {
+                return ((this.i1 == a.i1) && (this.s1.Equals(a.s1)));
+            }
+        }
+        public override int GetHashCode()
+        {
+            return i1.GetHashCode() + s1.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "DADTestA(" + i1 + "," + "\"" + s1 + "\")";
+        }
+
+        
+    }
+
+    [Serializable]
+    public class DADTestB
+    {
+        public int i1;
+        public string s1;
+        public int i2;
+
+        public DADTestB(int pi1, string ps1, int pi2)
+        {
+            i1 = pi1;
+            s1 = ps1;
+            i2 = pi2;
+        }
+
+        public override bool Equals(object o)
+        {
+
+            StringEmulator sm = o as StringEmulator; //Test WILDCARD
+            if (sm != null)
+            {
+                if (sm.GetString().Equals("*"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            DADTestB b = o as DADTestB;
+            if (b == null)
+            {
+                return false;
+            }
+            else
+            {
+                return ((this.i1 == b.i1) && (this.s1.Equals(b.s1)) && (this.i2 == b.i2));
+            }
+        }
+        public override int GetHashCode()
+        {
+            return i1.GetHashCode() + s1.GetHashCode() + i2.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "DADTestB(" + i1 + "," + "\"" + s1 + "\"" +"," + "\"" + i2 + ")";
+        }
+
+    }
+
+    [Serializable]
+    public class DADTestC
+    {
+        public int i1;
+        public string s1;
+        public string s2;
+
+        public DADTestC(int pi1, string ps1, string ps2)
+        {
+            i1 = pi1;
+            s1 = ps1;
+            s2 = ps2;
+        }
+
+        public override bool Equals(object o)
+        {
+            StringEmulator sm = o as StringEmulator; //Test WILDCARD
+            if (sm != null)
+            {
+                if (sm.GetString().Equals("*"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            DADTestC c = o as DADTestC;
+            if (c == null)
+            {
+                return false;
+            }
+            else
+            {
+                return ((this.i1 == c.i1) && (this.s1.Equals(c.s1)) && (this.s2.Equals(c.s2)));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return i1.GetHashCode() + s1.GetHashCode() + s2.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "DADTestC(" + i1 + "," + "\"" + s1 + "\"" + "," + "\"" + s2 + "\"" + ")";
+        }
+
+    }
+
+
+
+
+
+    /// <summary>
+    /// Simular String com ""
+    /// </summary>
+    [Serializable]
+    public class StringEmulator
+    {
+        public string s1;
+        
+
+        public StringEmulator(string ps1)
+        {
+            
+            s1 = ps1;
+           
+        }
+
+        public int GetSize()
+        {
+            return s1.Length;
+        }
+
+        public string GetString()
+        {
+            return s1;
+        }
+        public override bool Equals(object o)
+        {
+            StringEmulator a = o as StringEmulator;
+            if (a == null)
+            {
+                return false;
+            }
+            else
+            {
+                if ( this.s1.Equals(a.s1)==true)
+                {
+                    return true;
+                }
+                else
+                {
+                    string pattern = "";
+
+                    //Console.WriteLine("DEBUG: {0}", a.GetString());
+                    if ( a.GetString()[0]=='*' ) //In the beginning
+                    {
+                        if (a.GetSize() == 1) //all
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            pattern += a.GetString().Substring(1) + "$";
+                            Regex rx = new Regex(@pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                            MatchCollection matches = rx.Matches(s1);
+                            if (matches.Count > 0)
+                            {
+                                return true;
+                            }
+
+                        }
+                    }
+                    if ( a.GetString()[a.GetSize() - 1]=='*' ) //In the end
+                    {
+                        pattern += "^" + a.GetString().Substring(0, a.GetSize() - 2);
+                        Regex rx = new Regex(@pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                        MatchCollection matches = rx.Matches(s1);
+                        if (matches.Count > 0)
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+        public override int GetHashCode()
+        {
+            return s1.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "\"" + s1 + "\"";
+        }
+
+    }
+}
---
+++ AllCodes/Code_test_version/TupleSpace/TupleSpace/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleSpace
+{
+    
+
+
+
+
+    /// <summary>
+    /// Represents Each Tuple
+    /// </summary>
+    class MyTuple
+    {
+        List<object> ObjTuple;
+        
+        public MyTuple(object[] obj)
+        {
+            ObjTuple = new List<object>();
+
+            for (int i = 0; i<obj.Length; ++i)
+            {
+                ObjTuple.Add(obj[i]);
+            }
+        }
+
+        
+        public int GetSize()
+        {
+            return ObjTuple.Count;
+        }
+
+        public object GetValue(int pos)
+        {
+            return ObjTuple[pos];
+        }
+
+        
+        
+        /// <summary>
+        /// Completely Equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            MyTuple a = obj as MyTuple;
+            if (a == null || this.GetSize()!=a.GetSize() )
+            {
+                return false;
+            }
+            else
+            {
+                for(int i=0; i<this.GetSize(); ++i)
+                {
+                    if( this.GetValue(i).Equals(a.GetValue(i)) )
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string tmp = "<";
+
+            for (int i=0; i<ObjTuple.Count; ++i)
+            {
+                tmp += ObjTuple[i].ToString();
+                if(i+1 < ObjTuple.Count)
+                {
+                    tmp += ",";
+                }
+            }         
+            return tmp + ">";
+        }
+
+        
+    }
+
+
+    /// <summary>
+    /// Space of Tuples
+    /// </summary>
+    class TupleSpace
+    {
+        List<MyTuple> mySpace;
+
+        public TupleSpace()
+        {
+            mySpace = new List<MyTuple>();
+        }
+
+        public void add(MyTuple mt)
+        {
+            mySpace.Add( mt );
+        }
+
+        
+
+        public object read(MyTuple mt)
+        {
+   
+            for (int i=0; i<mySpace.Count; ++i)
+            {
+                if (mySpace[i].Equals(mt)) //Finds the first one. Returns
+                {
+                    return mySpace[i];
+                }
+                
+            }
+            return false;
+        }
+
+        public object take(MyTuple mt)
+        {
+
+            for (int i = 0; i < mySpace.Count; ++i)
+            {
+                if (mySpace[i].Equals(mt)) //Finds the first one. Returns
+                {
+                    object tmp = mySpace[i];
+                    mySpace.Remove(mySpace[i]);
+                    return tmp;
+
+                }
+
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string tmp = "";
+            foreach(MyTuple i in mySpace)
+            {
+                tmp += i + "\n";
+            }
+            return tmp;
+        }
+
+        
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TupleSpace ts = new TupleSpace();
+
+
+            
+
+            object[] o1 = { new StringEmulator("a"), new DADTestA(1, "b") };
+            MyTuple um = new MyTuple( o1 );
+            //Console.WriteLine("Tuplo 1:" + um);
+
+            ts.add( um );
+
+            object[] o2 = { new DADTestA(1, "a"), new DADTestB(1, "c", 2), new DADTestC(1, "b", "d") };
+            MyTuple dois = new MyTuple(o2);
+            //Console.WriteLine("Tuplo 2:" + dois);
+
+            ts.add(dois);
+            //Console.WriteLine(ts);
+
+            object[] o3 = { new StringEmulator("a") };
+            MyTuple tres = new MyTuple(o3);
+            ts.add(tres);
+            Console.WriteLine("Espaço de tuplos:");
+            Console.WriteLine(ts);
+
+            Console.WriteLine( "READ {0}: {1}", dois, ts.read(dois));
+            Console.WriteLine("READ {0}: {1}", um, ts.read(um));
+
+            //object[] c = { new StringEmulator("a") };
+            object[] o4 = { new StringEmulator("*") };
+            MyTuple quatro = new MyTuple(o4);
+            Console.WriteLine("READ {0}: {1}", quatro, ts.read(quatro));
+
+            object[] o5 = { new StringEmulator("*"), new DADTestB(1, "c", 2), new DADTestC(1, "b", "d") };
+            MyTuple cinco = new MyTuple(o5);
+            Console.WriteLine("READ {0}: {1}", cinco, ts.read(cinco));
+
+            ts.take(tres);
+            Console.WriteLine("Espaço de tuplos:");
+            Console.WriteLine(ts);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
